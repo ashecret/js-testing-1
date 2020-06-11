c5b7ad26c054cbd727589bfecce0e7afb64f6f76
Ash Alam <ashecret@users.noreply.github.com>
2020-06-11 22:35:40 +0100

    initial

---
+++ src/sample.js
@@ -0,0 +1,12 @@
+const greeting = 'hello'
+console.log(`${greting} world!`)
+
+const name = 'John Doe'
+typeof name === 'strng'
+
+[('a', 'b', 'c')].forEach(x => console.log(x))
+
+if (!'serviceWorker' in navigator) {
+  // you have an old browser :-(
+}
+
