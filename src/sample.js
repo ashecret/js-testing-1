d9482705637238af1794a2ad8266b14877b133a8
Ash Alam <ashecret@users.noreply.github.com>
2020-06-11 23:36:21 +0100

    changes to the file to trigger error

--- src/sample.js
+++ src/sample.js
@@ -4,9 +4,8 @@ console.log(`${greting} world!`)
 const name = 'John Doe'
 typeof name === 'strng'
 
-[('a', 'b', 'c')].forEach(x => console.log(x))
-
 if (!'serviceWorker' in navigator) {
-  // you have an old browser :-(
+  // old browser :-(
 }
 
+[('a', 'b', 'c')].forEach(x => console.log(x))
