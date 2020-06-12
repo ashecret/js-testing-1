d03f7028ae6f8fd32bd5ca491c52ccc1e5a01303
Ash Alam <ashecret@users.noreply.github.com>
2020-06-12 01:40:03 +0100

    husky starter

---
+++ src/ts-example.ts
@@ -0,0 +1,21 @@
+function add(a: number, b: number): number {
+  return a + b
+}
+interface User {
+  name: {
+    first: string
+    middle: string
+    last: string
+  }
+}
+
+function getFullname(user: User) {
+  const {
+    name: { first, middle, last },
+  } = user
+  return [first, middle, last].filter(Boolean).join('')
+}
+
+add(1, 2)
+
+getFullname({ name: { first: 'Joe', middle: 'Bud', last: 'Matthews' } })
